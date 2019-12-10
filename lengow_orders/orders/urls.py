2421547607871a949bcab37e91a853ad851c4434
AR <ar.anthonyrobin@gmail.com>
2019-12-10 21:58:38 +0100

    Modification de la page affichant toutes les commandes afin d'avoir un espace de tri.

--- lengow_orders/orders/urls.py
+++ lengow_orders/orders/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 app_name = 'orders'
 urlpatterns = [
     path('', views.orders, name='orders'),
-    path('<str:order_id>/', views.order, name='order'),
+    path('<str:order_id>', views.order, name='order'),
 ]
--- lengow_orders/orders/views.py
+++ lengow_orders/orders/views.py
@@ -1,11 +1,38 @@
 from django.shortcuts import render, get_object_or_404
-
 from .models import Order
 
 
 def orders(request):
-    orders_list = Order.objects.all()
-    context = {'orders_list': orders_list}
+    context = dict()
+
+    if request.method == "POST":
+        marketplace = request.POST.get('marketplace')
+        idflux = request.POST.get('idflux')
+        day = request.POST.get('day')
+        month = request.POST.get('month')
+        year = request.POST.get('year')
+
+        orders_list = Order.objects.all()
+
+        if marketplace != '':
+            orders_list = orders_list.filter(marketplace__icontains=marketplace)
+            context['marketplace'] = marketplace
+        if idflux != '':
+            orders_list = orders_list.filter(idFlux=idflux)
+            context['idflux'] = idflux
+        if day != '':
+            orders_list = orders_list.filter(order_purchase_date__day=day)
+            context['day'] = day
+        if month != '':
+            orders_list = orders_list.filter(order_purchase_date__month=month)
+            context['month'] = month
+        if year != '':
+            orders_list = orders_list.filter(order_purchase_date__year=year)
+            context['year'] = year
+    else:
+        orders_list = Order.objects.all()
+
+    context['orders_list'] = orders_list
     return render(request, 'orders/orders.html', context)
 
 
